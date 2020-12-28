cdc8bfa1e127e51d3b1cc4f931ab2bd6ed6d193a
Leticia Riera <leticiariera22@gmail.com>
2020-12-28 10:19:54 -0300

    Workshop 1

---
+++ js/db.js
@@ -0,0 +1,30 @@
+const DATOS = [{
+    "id": 1,
+    "tipoWeb": "Institucional",
+    "paginas": 5,
+    "diseno": "Plantilla",
+    "precio": 20000,
+},
+{
+    "id": 2,
+    "tipoWeb": "Blog",
+    "paginas": 100,
+    "diseno": "A medida",
+    "precio": 60000,
+
+},
+{
+    "id": 3,
+    "tipoWeb": "E-commerce",
+    "paginas": 1000,
+    "diseno": "Plantilla",
+    "precio": 100000,
+},
+{
+    "id": 4,
+    "tipoWeb": "Web a medida",
+    "paginas": 10,
+    "diseno": "A medida",
+    "precio": 70000,
+}];
+
--- js/main.js
+++ js/main.js
@@ -18,39 +18,6 @@ let cuartoProducto = document.getElementById("productoCuatro");
 cuartoProducto.firstElementChild.innerHTML = "A medida"
 
 
-
-
-const DATOS = [{
-    "id": 1,
-    "tipoWeb": "Institucional",
-    "paginas": 5,
-    "diseno": "Plantilla",
-    "precio": 20000,
-},
-{
-    "id": 2,
-    "tipoWeb": "Blog",
-    "paginas": 100,
-    "diseno": "A medida",
-    "precio": 60000,
-
-},
-{
-    "id": 3,
-    "tipoWeb": "E-commerce",
-    "paginas": 1000,
-    "diseno": "Plantilla",
-    "precio": 100000,
-},
-{
-    "id": 4,
-    "tipoWeb": "Web a medida",
-    "paginas": 10,
-    "diseno": "A medida",
-    "precio": 70000,
-}];
-
-
 class Presupuesto {
     constructor (datos){
         this.id = parseInt(datos.id);
