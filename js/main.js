caacd47f020a14a3b64efeaa902ccbf1c08fb337
Leticia Riera <leticiariera22@gmail.com>
2021-01-16 20:46:10 -0300

    Mostrar elementos con JSON

--- js/main.js
+++ js/main.js
@@ -1,80 +1,126 @@
 // SIMULADOR PARA REALIZAR PRESUPUESTOS DE PÁGINAS WEB. Al hacer clic en una imagen (pregunta 1) muestra las siguiente 
 
-// const TIPOWEB = "data/web.json";
+// const TIPOWEB = [];
 
-$.getJSON("data/web.json", (respuesta)=> {
-    let dato = respuesta;
-    console.log(respuesta);
-}
-);
+// $.getJSON("data/web.json", (respuesta, estado)=> {
+//      let TIPOWEB = respuesta;
+//     console.log(TIPOWEB);
+//     for (const web of TIPOWEB){
+//         $("#preguntaUno").append(`<div class="row" id="fila"><img src="${web.avatar}"</div>`);
+//         $("preguntaUno").append
 
-$.get("presupuesto.html", function (data, status){
-    console.log(data);
-    console.log(status);
-})
 
+
+
+        
+//     }
+// }
+// );
+
+
+
+
+const TIPOWEB = [];
 
 $("#resumen").hide();
 
-for (let index = 0; index < TIPOWEB.length; index+=4){
-    $("#preguntaUno").append(`<div class="row" id="fila1${TIPOWEB[index].id}"></div><hr>`);
-    $(`#fila1${TIPOWEB[index].id}`).append(componente1(TIPOWEB[index]));
-    $(`#fila1${TIPOWEB[index].id}`).append(componente1(TIPOWEB[index+1]));
-    $(`#fila1${TIPOWEB[index].id}`).append(componente1(TIPOWEB[index+2]));
-    $(`#fila1${TIPOWEB[index].id}`).append(componente1(TIPOWEB[index+3]));
+$.getJSON("data/web.json", (respuesta, estado)=> {
+     let TIPOWEB = respuesta;
+    console.log({TIPOWEB});
+    for (const web of TIPOWEB){
+        $("#preguntaUno").append(`<div class="col-md-3 float-left">
+                                    <div class="project text-center">
+                                        <img class="img__tiposweb"  src="${web.avatar}"/>
+                                    </div>
+                                    <button id="pregunta${web.web}" class="btn btn-primary">${web.web}</button>
+                                    </div>`);
+     
+        $('button[id^="pregunta"]').click((e) => {
+            
+            let respuestaUno = e.target.id.substr(8);
+
+            $('#resumen').html(`<h2>Resumen presupuesto</h2> <br>
+            <b>Tipo de web:</b> ${respuestaUno}`);
+            $("#resumen").fadeIn(2000);
+
+            console.log(respuestaUno);
+        }
+        )  
+
+
+
+        
+    }
 }
+);
+// $('button[id^="pregunta"]').click((e) => {
 
-$('button[id^="pregunta"]').click((e) => {
+//     let respuestaUno = e.target.id.substr(8).toUpperCase();
+//     $("#resumen").html(`<h2>Resumen presupuesto</h2> <br>
+//     <b>Tipo de web:</b> ${respuestaUno}`);
+//     $("#resumen").fadeIn(2000);
 
-    let respuestaUno = e.target.id.substr(8).toUpperCase();
-    $("#resumen").html(`<h2>Resumen presupuesto</h2> <br>
-    <b>Tipo de web:</b> ${respuestaUno}`);
-    $("#resumen").fadeIn(2000);
+// $("#resumen").hide();
 
+// for (let index = 0; index < TIPOWEB.length; index+=4){
+    // $("#preguntaUno").append(`<div class="row" id="fila1${TIPOWEB[index].id}"></div><hr>`);
+    // $(`#fila1${TIPOWEB[index].id}`).append(componente1(TIPOWEB[index]));
+    // $(`#fila1${TIPOWEB[index].id}`).append(componente1(TIPOWEB[index+1]));
+    // $(`#fila1${TIPOWEB[index].id}`).append(componente1(TIPOWEB[index+2]));
+    // $(`#fila1${TIPOWEB[index].id}`).append(componente1(TIPOWEB[index+3]));
+// }
 
-})
+// $('button[id^="pregunta"]').click((e) => {
 
-function componente1 (pregunta1){
-    return `
-            <div class="col-md-3">
-                <div class="project text-center">
-                    <img class="img__tiposweb" src="${pregunta1.avatar}">
-                    <div>
-                        <button id="pregunta${pregunta1.web}" class="btn btn-primary">${pregunta1.web}</button>
-                    </div>
-                </div>
-            </div>
-              `; 
-}
-
-
-for (let preg2 = 0; preg2 < DISENO.length; preg2+=4){
-    $("#preguntaDos").append(`<div class="row" id="fila2${DISENO[preg2].id}"></div><hr>`);
-    $(`#fila2${DISENO[preg2].id}`).append(componente2(DISENO[preg2]));
-    $(`#fila2${DISENO[preg2].id}`).append(componente2(DISENO[preg2+1]));
-    $(`#fila2${DISENO[preg2].id}`).append(componente2(DISENO[preg2+2]));
-    $(`#fila2${DISENO[preg2].id}`).append(componente2(DISENO[preg2+3]));
-}
-
-$('button[id^="pregunta2"]').click((e) => {
-
-    let respuestaDos = e.target.id.substr(9).toUpperCase();
-    $("#resumen").html(`<h2>Resumen presupuesto</h2> <br>
-    <b>Tipo de diseño:</b> ${respuestaDos}`);
-    $("#resumen").fadeIn(2000);
+//     let respuestaUno = e.target.id.substr(8).toUpperCase();
+//     $("#resumen").html(`<h2>Resumen presupuesto</h2> <br>
+//     <b>Tipo de web:</b> ${respuestaUno}`);
+//     $("#resumen").fadeIn(2000);
 
 
-})
+// })
 
-function componente2 (pregunta2){
-    return `
-            <div class="col-md-3">
-                <div class="project text-center">
-                    <img class="img__tiposweb" src="${pregunta2.avatar}">
-                    <div>
-                        <button id="pregunta2${pregunta2.tipoDiseno}" class="btn btn-primary">${pregunta2.tipoDiseno}</button>
-                    </div>
-                </div>
-            </div>
-              `; 
-}
+// function componente1 (respuesta){
+//     return `
+//             <div class="col-md-3">
+//                 <div class="project text-center">
+//                     <img class="img__tiposweb" src="${respuesta.avatar}"/>
+//                     <div>
+//                         <button id="pregunta${respuesta.web}" class="btn btn-primary">${respuesta.web}</button>
+//                     </div>
+//                 </div>
+//             </div>
+//               `; 
+// }
+
+
+// for (let index = 0; index < DISENO.length; index+=4){
+//     $("#preguntaDos").append(`<div class="row" id="fila2${DISENO[index].id}"></div><hr>`);
+//     $(`#fila2${DISENO[index].id}`).append(componente2(DISENO[index]));
+//     $(`#fila2${DISENO[index].id}`).append(componente2(DISENO[index+1]));
+//     $(`#fila2${DISENO[index].id}`).append(componente2(DISENO[index+2]));
+//     $(`#fila2${DISENO[index].id}`).append(componente2(DISENO[index+3]));
+// }
+
+// $('button[id^="pregunta2"]').click((e) => {
+
+//     let respuestaDos = e.target.id.substr(9).toUpperCase();
+//     $("#resumen").html(`<h2>Resumen presupuesto</h2> <br>
+//     <b>Tipo de diseño:</b> ${respuestaDos}`);
+//     $("#resumen").fadeIn(2000);
+
+
+// })
+
+// function componente2 (pregunta2){
+//     return `
+//             <div class="col-md-3">
+//                 <div class="project text-center">
+//                     <img class="img__tiposweb" src="${pregunta2.avatar}">
+//                     <div>
+//                         <button id="pregunta2${pregunta2.tipoDiseno}" class="btn btn-primary">${pregunta2.tipoDiseno}</button>
+//                     </div>
+//                 </div>
+//             </div>
+//               `; 
+// }
